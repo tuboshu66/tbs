676a32e8926d122a1110839ec531ab99b27dfc21
tuboshu66 <tuboshu66@protonmail.com>
2024-09-24 15:25:04 +0800

    自动更新提交 Tue Sep 24 15:25:04 CST 2024

---
+++ test/test6/tbs/bymyDDNS.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Cloudflare API 信息
+CF_API="https://api.cloudflare.com/client/v4/zones"
+ZONE_ID="eea63961734d3988f1365492d0d0f2ad"  # 你的 Cloudflare 域名 Zone ID 区域ID
+RECORD_ID="8a930419ce37918aa36c25fe8ee77742"  # 你要更新的 DNS 记录的 ID
+AUTH_EMAIL="qc3277734@gmail.com"  # 你的 Cloudflare 账户邮箱
+AUTH_KEY="fTdSRwO4O0v26qNME1t0j5H-H-dyBqQ9OY8Dzt51"  # 你的 Cloudflare API Token
+DOMAIN="bymy.tfvou.com"  # 你想更新的域名或子域名
+IP=$(curl -s ifconfig.me)  # 获取当前公网 IP
+
+# 更新 DNS 记录
+RESPONSE=$(curl -s -X PUT "$CF_API/$ZONE_ID/dns_records/$RECORD_ID" \
+     -H "Authorization: Bearer $AUTH_KEY" \
+     -H "Content-Type: application/json" \
+     --data "{\"type\":\"A\",\"name\":\"$DOMAIN\",\"content\":\"$IP\",\"ttl\":120,\"proxied\":false}")
+
+# 检查是否成功更新
+if [[ $RESPONSE == *"\"success\":true"* ]]; then
+    echo "DNS 记录已成功更新为 $IP"
+else
+    echo "DNS 记录更新失败"
+fi
