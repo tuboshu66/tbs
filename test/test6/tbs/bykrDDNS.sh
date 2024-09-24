9aa07a992c780eb782f3b4f70d6d9e5eebf49e78
tuboshu66 <tuboshu66@protonmail.com>
2024-09-24 16:20:41 +0800

    自动更新提交 Tue Sep 24 16:20:41 CST 2024

--- test/test6/tbs/bykrDDNS.sh
+++ test/test6/tbs/bykrDDNS.sh
@@ -7,7 +7,7 @@ RECORD_ID="cd02c7bee64112df052a19932817a0cd"  # 你要更新的 DNS 记录的 ID
 AUTH_EMAIL="qc3277734@gmail.com"  # 你的 Cloudflare 账户邮箱
 AUTH_KEY="3773452a14b1f9f0073ed467fa3c6efc79d9d"  # 你的 Cloudflare API Token
 DOMAIN="bykr.tfvou.com"  # 你想更新的域名或子域名
-IP=$(curl -s ifconfig.me)  # 获取当前公网 IP
+IP=$(curl -s -4 ifconfig.me)  # 获取当前公网 IP
 
 # 更新 DNS 记录
 RESPONSE=$(curl -s -X PUT "$CF_API/$ZONE_ID/dns_records/$RECORD_ID" \
