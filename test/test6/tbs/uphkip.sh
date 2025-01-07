112f2b826252066c108351fd00fa776b94e22ac4
tuboshu66 <tuboshu66@protonmail.com>
2025-01-07 12:53:37 +0800

    自动更新提交 Tue Jan  7 12:53:37 CST 2025

--- test/test6/tbs/uphkip.sh
+++ test/test6/tbs/uphkip.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# Telegram 通知函数
+function TG_BOT() {
+  export TGSEND_TOKEN="5688173096:AAFyqcmKdfa1TaaBMnXNRgs7DGCZYQz5iS8"
+  export TGSEND_CHATID="1088857444"
+  curl -s -k "https://thingproxy.freeboard.io/fetch/https://api.telegram.org/bot$TGSEND_TOKEN/sendMessage" \
+      --data-urlencode "chat_id=$TGSEND_CHATID" \
+      --data-urlencode "text=$TG_MESSAGE" \
+      > /dev/null &
+}
+
 # 获取当前域名的IP
 new_ip=$(nslookup awshk1.tfvou.com | grep 'Address:' | tail -n 1 | awk '{print $2}')
 
 # 确保获取到的 IP 地址不是空值
 if [ -z "$new_ip" ]; then
-  echo "未获取到新的IP地址，请检查域名解析是否正确。"
+  TG_MESSAGE="未获取到新的IP地址，请检查域名解析是否正确。"
+  TG_BOT
+  echo "$TG_MESSAGE"
   exit 1
 fi
 
@@ -13,13 +25,17 @@ fi
 if [ -f /root/tbs/test/test6/tbs/hkip1.txt ]; then
   old_ip=$(cat /root/tbs/test/test6/tbs/hkip1.txt)
 else
-  echo "hkip1.txt 文件不存在，请检查路径。"
+  TG_MESSAGE="hkip1.txt 文件不存在，请检查路径。"
+  TG_BOT
+  echo "$TG_MESSAGE"
   exit 1
 fi
 
 # 确保原有 IP 地址不是空值
 if [ -z "$old_ip" ]; then
-  echo "原有 IP 地址为空，请检查 hkip1.txt 文件内容。"
+  TG_MESSAGE="原有 IP 地址为空，请检查 hkip1.txt 文件内容。"
+  TG_BOT
+  echo "$TG_MESSAGE"
   exit 1
 fi
 
@@ -33,17 +49,25 @@ if [ "$new_ip" != "$old_ip" ]; then
   if sed -i "s/$old_ip/$new_ip/g" /root/tbs/test/test6/tbs/tbs.conf; then
     echo "tbs.conf 文件中的 IP 更新成功。"
   else
-    echo "更新 tbs.conf 文件中的 IP 失败。" && exit 1
+    TG_MESSAGE="更新 tbs.conf 文件中的 IP 失败。"
+    TG_BOT
+    echo "$TG_MESSAGE"
+    exit 1
   fi
 
   # 更新 hkip1.txt 中的 IP
   if echo "$new_ip" > /root/tbs/test/test6/tbs/hkip1.txt; then
-    echo "hkip1.txt 文件中的 IP 更新成功。"
+    TG_MESSAGE="IP 更新成功: $old_ip -> $new_ip"
+    TG_BOT
+    echo "$TG_MESSAGE"
   else
-    echo "更新 hkip1.txt 文件中的 IP 失败。" && exit 1
+    TG_MESSAGE="更新 hkip1.txt 文件中的 IP 失败。"
+    TG_BOT
+    echo "$TG_MESSAGE"
+    exit 1
   fi
-
-  echo "IP 更新成功: $old_ip -> $new_ip"
 else
-  echo "IP 无变化"
+  TG_MESSAGE="IP 无变化"
+  TG_BOT
+  echo "$TG_MESSAGE"
 fi
